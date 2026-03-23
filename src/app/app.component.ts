66ebffec23a1160e4fb11cfd83d33c8967e3823a
az1_kphongsavath <az1_KPhongsavath@vscocorp.com>
2026-03-23 15:55:09 -0400

    v1

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+import { TreeGridExcelStyleFilteringSample1Component } from "./tree-grid-excel-style-filtering-sample-1/tree-grid-excel-style-filtering-sample-1.component";
+
+
+@Component({
+    selector: "app-root",
+    styleUrls: ["./app.component.scss"],
+    templateUrl: "./app.component.html",
+    imports: [TreeGridExcelStyleFilteringSample1Component]
+})
+export class AppComponent {}
---
+++ src/app/tree-grid-excel-style-filtering-sample-1/tree-grid-excel-style-filtering-sample-1.component.ts
@@ -0,0 +1,74 @@
+import { Component, HostBinding, OnInit, ViewChild } from '@angular/core';
+import { IgxTreeGridComponent } from 'igniteui-angular/grids/tree-grid';
+import { TreeGridFilteringStrategy } from 'igniteui-angular/core';
+import { IgxButtonGroupComponent } from 'igniteui-angular/button-group';
+import { IgxCellTemplateDirective, IgxColumnComponent, IgxGridToolbarActionsComponent, IgxGridToolbarComponent, IgxGridToolbarHidingComponent, IgxGridToolbarPinningComponent } from 'igniteui-angular/grids/core';
+import { ORDERS_DATA, ORDERS_DATA_NESTED } from '../data/orders';
+import { IgxPreventDocumentScrollDirective } from '../directives/prevent-scroll.directive';
+
+
+@Component({
+    selector: 'app-tree-grid-excel-style-filtering-sample-1',
+    styleUrls: ['./tree-grid-excel-style-filtering-sample-1.component.scss'],
+    templateUrl: 'tree-grid-excel-style-filtering-sample-1.component.html',
+    imports: [IgxButtonGroupComponent, IgxTreeGridComponent, IgxPreventDocumentScrollDirective, IgxGridToolbarComponent, IgxGridToolbarActionsComponent, IgxGridToolbarHidingComponent, IgxGridToolbarPinningComponent, IgxColumnComponent, IgxCellTemplateDirective]
+})
+
+export class TreeGridExcelStyleFilteringSample1Component implements OnInit {
+    @ViewChild('treegrid1', { read: IgxTreeGridComponent, static: true })
+    public treegrid1: IgxTreeGridComponent;
+
+    public data: any[];
+
+    public options = {
+        digitsInfo: '1.2-2',
+        currencyCode: 'USD'
+    };
+    public formatOptions = this.options;
+
+    public filterStrategy = new TreeGridFilteringStrategy(['ID', 'Name']);
+
+    public size = 'large';
+    public sizes: any[];
+
+    constructor() {
+    }
+    public ngOnInit(): void {
+        this.data = ORDERS_DATA_NESTED;
+        this.sizes = [
+            {
+                label: 'small',
+                selected: this.size === 'small',
+                togglable: true
+            },
+            {
+                label: 'medium',
+                selected: this.size === 'medium',
+                togglable: true
+            },
+            {
+                label: 'large',
+                selected: this.size === 'large',
+                togglable: true
+            }
+        ];
+    }
+
+    @HostBinding('style.--ig-size')
+    protected get sizeStyle() {
+        return `var(--ig-size-${this.size})`;
+    }
+
+    public selectSize(event: any) {
+        this.size = this.sizes[event.index].label;
+        this.treegrid1.reflow();
+    }
+
+    public formatDate(val) {
+        if (val !== 'Select All') {
+            return new Intl.DateTimeFormat('en-US').format(val);
+        } else {
+            return val;
+        }
+    }
+}
